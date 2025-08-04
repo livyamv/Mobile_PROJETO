28e76327ded5825d56dd6bfdb66077ea5add1c85
livyamv <liviareis2702@gmail.com>
2025-08-04 16:09:55 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Home from "./screens/Home";
+import Login from "./screens/Login";
+import Cadastro from "./screens/Cadastro";
 
-function App() {
-  const [count, setCount] = useState(0)
+const Stack = createStackNavigator();
 
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Login">{() => <Login />}</Stack.Screen>
+        <Stack.Screen name="Cadastro">{() => <Cadastro />}</Stack.Screen>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
-
-export default App
---
+++ src/screens/Login.js
@@ -0,0 +1,119 @@
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Alert,
+  StyleSheet,
+  Button,
+} from "react-native";
+import api from "../axios/api";
+import { Ionicons } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
+import * as SecureStore from "expo-secure-store";
+
+export default function Login() {
+  const navigation = useNavigation();
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+    showPassaword: false,
+  });
+
+  async function saveToken(token) {
+    await SecureStore.setItemAsync("token", token);
+  }
+
+  async function handleLogin() {
+    await api.postLogin(user).then(
+      (response) => {
+        Alert.alert("OK", response.data.message);
+
+        saveToken(response.data.token);
+        navigation.navigate("EventosScreens");
+      },
+      (error) => {
+        console.log(error.response.data.error);
+        Alert.alert("Erro", error.response.data.error);
+      }
+    );
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}> Faça Login</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        value={user.email}
+        onChangeText={(value) => {
+          setUser({ ...user, email: value });
+        }}
+      />
+      <View style={styles.passwordContainer}>
+        <TextInput
+          style={styles.passwordInput}
+          placeholder="Senha"
+          value={user.password}
+          secureTextEntry={user.showPassword}
+          onChangeText={(value) => {
+            setUser({ ...user, password: value });
+          }}
+        />
+        <TouchableOpacity
+          onPress={() => setUser({ ...user, showPassword: !user.showPassword })}
+        >
+          <Ionicons
+            name={user.showPassword ? "eye-off" : "eye"}
+            size={24}
+            color="black"
+          />
+        </TouchableOpacity>
+      </View>
+
+      <TouchableOpacity onPress={handleLogin} style={styles.button}>
+        <Text>Entrar</Text>
+      </TouchableOpacity>
+      <Button
+        title="Cadastro"
+        onPress={() => navigation.navigate("Cadastro")}
+        color="#C77FFF"
+      />
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: "bold",
+  },
+  input: {
+    width: "100%",
+    height: 40,
+    borderBottomWidth: 1,
+    marginBottom: 20,
+    paddingHorizontal: 10,
+  },
+  button: {
+    backgroundColor: "#C77FFF",
+    padding: 10,
+    borderRadius: 5,
+  },
+  passwordContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    width: "100%",
+    borderBottomWidth: 1,
+    paddingRight: 10,
+  },
+  passwordInput: {
+    flex: 1,
+    height: 40,
+  },
+});
